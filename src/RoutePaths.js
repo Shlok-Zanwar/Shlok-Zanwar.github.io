a0d3cb2c5819b8e9b2d71167acad7cffceb7b3f5
Shlok-Zanwar <Shlok-Zanwar@users.noreply.github.com>
2021-01-25 08:56:12 +0530

    For multipage support

--- src/RoutePaths.js
+++ src/RoutePaths.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useEffect } from 'react'
+import {BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import BlogTemplate from './BlogTemplates/BlogTemplate';
 import Home from './HomeComponents/Home';
 import MyNavbar from "./MyNavbar";
 import TodoApp from "./TodoAppComponents/TodoApp"
 
 function RoutePaths() {
+
+    var forUseEffect;
+    useEffect(() => {
+        localStorage.removeItem("redirectTo");
+    }, [forUseEffect])
+
     return (
         <Router>
             <MyNavbar />
@@ -21,7 +27,13 @@ function RoutePaths() {
                 <Route path="/todo-app">
                     <TodoApp />
                 </Route>
+
                 <Route path="/">
+                    {
+                        localStorage.getItem('redirectTo') ? <Redirect to={localStorage.getItem('redirectTo')} /> 
+                        : null
+                        
+                    }
                     <Home />
                 </Route>
 
