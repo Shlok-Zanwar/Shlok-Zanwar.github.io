baeecbecbaa90b74169d7f94055496ab97c269e5
Shlok-Zanwar <Shlok-Zanwar@users.noreply.github.com>
2021-02-18 09:58:34 +0530

    Changed update button

--- src/TodoAppComponents/components/TodoForm.js
+++ src/TodoAppComponents/components/TodoForm.js
@@ -53,7 +53,7 @@ function TodoForm(props) {
                 onChange={handleChange}
                 ref={inputRef}
             />
-            <button className="todo-button edit">Update Todo</button>
+            <button className="todo-button edit">Update</button>
             </>
             ) : (
             <>
