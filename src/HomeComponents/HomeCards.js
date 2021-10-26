c928b331675a5b01044e32a43c813093b17b1748
Shlok-Zanwar <shlokzanwar14@gmail.com>
2021-10-26 22:12:53 +0530

    Added Code rooms card

--- src/HomeComponents/HomeCards.js
+++ src/HomeComponents/HomeCards.js
@@ -1,74 +1,69 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import Masonry from 'react-masonry-css'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Masonry from "react-masonry-css";
 
 function HomeCards() {
-    const [cards] = useState(require('./CardDetails.json'))
+    const [cards] = useState(require("./CardDetails.json"));
 
     // const handleClickCards = (url) =>{
-    //     window.location.href = 
+    //     window.location.href =
     // }
 
-    const cardType = (type) => {
-        if(type === "Blog"){
-            return (
-                <div className="card-type green-border">
-                    {type}
-                </div>
-            )
+    const cardType = type => {
+        if (type === "Blog") {
+            return <div className="card-type green-border">{type}</div>;
+        } else if (type === "App") {
+            return <div className="card-type blue-border">{type}</div>;
+        } else if (type === "Model") {
+            return <div className="card-type yellow-border">{type}</div>;
         }
-        else if(type === "App"){
-            return (
-                <div className="card-type blue-border">
-                    {type}
-                </div>
-            )
-        }
-        else if(type === "Model"){
-            return (
-                <div className="card-type yellow-border">
-                    {type}
-                </div>
-            )
-        }
-    }
+    };
 
     const breakpoints = {
         default: 5,
         1600: 4,
         1250: 3,
         940: 2,
-        700: 1
+        700: 1,
     };
 
-    return ( 
-        <Masonry
-            breakpointCols={breakpoints}
-            className="my-masonry-grid"
-            columnClassName="my-masonry-grid_column"
-        >
-            { cards.map(card =>
-            <Link to={card.url} key={card.id}>
-                <div key={card.id} className="cards-box">
-                    <div className="cards-info">
-                        <div className="cards-title">
-                            {card.title}
+    const linkType = (card, component) => {
+        if (card.fullLink) {
+            return (
+                <a href={card.url} target="_blank" key={card.url}>
+                    {component}
+                </a>
+            );
+        } else {
+            return (
+                <Link to={card.url} key={card.id}>
+                    {component}
+                </Link>
+            );
+        }
+    };
+
+    return (
+        <Masonry breakpointCols={breakpoints} className="my-masonry-grid" columnClassName="my-masonry-grid_column">
+            {cards.map(card => (
+                <>
+                    {linkType(
+                        card,
+                        <div key={card.id} className="cards-box">
+                            <div className="cards-info">
+                                <div className="cards-title">{card.title}</div>
+                                <div className="cards-description">{card.description}</div>
+                            </div>
+                            <div className="card-bottom-bar">
+                                <div className="card-date ">{card.date}</div>
+                                {cardType(card.type)}
+                            </div>
                         </div>
-                        <div className="cards-description">
-                            {card.description}
-                        </div>
-                    </div>
-                    <div className="card-bottom-bar">
-                        <div className="card-date ">
-                            {card.date}
-                        </div>
-                        {cardType(card.type)}               
-                    </div>
-                </div>
-            </Link>
-            )}
+                    )}
+                </>
+            ))}
         </Masonry>
-    )
+    );
 }
 
-export default HomeCards
+export default HomeCards;
