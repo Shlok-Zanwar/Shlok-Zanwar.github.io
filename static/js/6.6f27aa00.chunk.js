496f58f239f9a35103970a5c3b8af3cd7fe3f8e3
Shlok-Zanwar <shlokzanwar14@gmail.com>
2023-02-18 00:12:53 +0530

    Updates

--- static/js/6.6f27aa00.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpshlok-zanwar"]=this["webpackJsonpshlok-zanwar"]||[]).push([[6],{585:function(t,a,h){"use strict";h.d(a,"a",(function(){return v}));var n=h(0);function v(t){return Object(n.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"g",attr:{},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M4 8h16v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8zm2 2v10h12V10H6zm3 2h2v6H9v-6zm4 0h2v6h-2v-6zM7 5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v2h5v2H2V5h5zm2-1v1h6V4H9z"}}]}]})(t)}}}]);
-//# sourceMappingURL=6.6f27aa00.chunk.js.map
